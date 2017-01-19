1395507c3c93d4f0fe5ce1306f106cb26ff7a5ce
Yifan Gu <gyf304@users.noreply.github.com>
2017-01-19 15:42:30 -0500

    Fixed conflict msg

--- src/helloworld/helloworld.py
+++ src/helloworld/helloworld.py
@@ -10,11 +10,8 @@ import antigravity
 def main():
     print """
     Hello World, from...
-<<<<<<< HEAD
     Joel S. Welling
     Yifan Gu
-=======
-    Joel S. Welling,
     Richard Zhao
 >>>>>>> b166633f556c2a5ab02bb74d7acd8232bf85cf13
     """
